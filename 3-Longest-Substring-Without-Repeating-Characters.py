21dacd716bf9a8616ac8ecd40b1b92822e5b1b4d
liuhongjiang <andrew.lhj@gmail.com>
2015-09-09 18:02:49 +0800

    Update solution for question 3 to improve performance

--- 3-Longest-Substring-Without-Repeating-Characters.py
+++ 3-Longest-Substring-Without-Repeating-Characters.py
@@ -8,22 +8,20 @@ class Solution(object):
         substring_map = {}
         start = 0
         for i in range(len(s)):
-            if s[i] in substring_map:
-                position = substring_map[s[i]]
+            position = substring_map.get(s[i])
+            if position is not None and position >= start:
                 length = i - start
                 start = position + 1
                 longest = max(length, longest)
-                new_map = {}
-                new_map.update((k, v) for k, v in substring_map.iteritems() if v > position)
-                substring_map = new_map
             substring_map[s[i]] = i
-        longest = max(len(substring_map), longest)
+        longest = max(len(s) - start, longest)
         return longest
 
 if __name__ == "__main__":
     solution = Solution()
-    print solution.lengthOfLongestSubstring("bbbbbb")
-    print solution.lengthOfLongestSubstring("abcabcbb")
-    print solution.lengthOfLongestSubstring("abcabcbb1234567")
-    print solution.lengthOfLongestSubstring("cdd")
-    print solution.lengthOfLongestSubstring("dvdf")
+    # print solution.lengthOfLongestSubstring("bbbbbb")
+    # print solution.lengthOfLongestSubstring("abcabcbb")
+    # print solution.lengthOfLongestSubstring("abcabcbb1234567")
+    # print solution.lengthOfLongestSubstring("cdd")
+    # print solution.lengthOfLongestSubstring("dvdf")
+    print solution.lengthOfLongestSubstring("pwwkew")
